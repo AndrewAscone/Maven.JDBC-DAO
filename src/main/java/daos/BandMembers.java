856aa5bd17f8b786f2574a815cf128e36c281318
AndrewAscone <andrew.ascone@gmail.com>
2022-12-06 09:42:39 -0500

    Created more classes, able to connect to database

---
+++ src/main/java/daos/BandMembers.java
@@ -0,0 +1,28 @@
+package daos;
+
+import models.BandMember;
+
+import java.util.List;
+
+public class BandMembers implements BandMembersDAO {
+
+    public BandMember findByID(int id) {
+        return null;
+    }
+
+    public List<BandMember> findAll() {
+        return null;
+    }
+
+    public BandMember update(BandMember dto) {
+        return null;
+    }
+
+    public BandMember create(BandMember dto) {
+        return null;
+    }
+
+    public void delete(int id) {
+
+    }
+}
